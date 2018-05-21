653f3a9d894d4e1cc96a2718f214743f1e930a24
Gimo <self@gimo.me>
2018-05-21 11:52:24 +0800

    chore: update setup.py

--- setup.py
+++ setup.py
@@ -8,13 +8,19 @@ setuptools.setup(
     description='OTCBTC SDK for Python',
     url='https://github.com/masakichi/otcbtc-client',
     packages=setuptools.find_packages(exclude=['tests']),
-    classifiers=(
-        'Programming Language :: Python :: 3',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent',
-    ),
+    license='MIT',
+    classifiers=('Intended Audience :: Developers',
+                 'License :: OSI Approved :: MIT License',
+                 'Operating System :: OS Independent',
+                 'Programming Language :: Python :: 2',
+                 'Programming Language :: Python :: 2.7',
+                 'Programming Language :: Python :: 3',
+                 'Programming Language :: Python :: 3.4',
+                 'Programming Language :: Python :: 3.5',
+                 'Programming Language :: Python :: 3.6',
+                 'Programming Language :: Python :: Implementation :: CPython',
+                 'Programming Language :: Python :: Implementation :: PyPy'),
     install_requires=[
-    'requests >=2.11, <3.0a0',
-    'future',
-    ]
-)
+        'requests >=2.11, <3.0a0',
+        'future',
+    ])
